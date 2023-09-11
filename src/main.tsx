68716e20818205999d3d8403d71cec96e4321053
Andreas Bagge <andreasbaggenielsen@gmail.com>
2023-09-11 23:00:41 +0200

    Added a comment

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+//npm run dev - to start the application
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
