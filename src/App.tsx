6f8f8bc4e5ed23c44be8ffa7b234bfe810ca47c7
Andreas Bagge <andreasbaggenielsen@gmail.com>
2023-09-25 23:03:18 +0200

    Added various data gathering apps to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import './App.css'
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Weather from "./components/weather";
 
 function App() {
+  const [lat, setLat] = useState<number | null>(null);
+  const [long, setLong] = useState<number | null>(null);
+  const [data, setData] = useState<weatherData | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [lat, long]);
 
   return (
     <>
-      
-      <p className=" bg-purple-700">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="App">
+        {data !== null && typeof data.main !== "undefined" ? (
+          <Weather weatherData={data} />
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
